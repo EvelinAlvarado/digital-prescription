f708420a3737a603295cf78be2c56acf9ba44cd2
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-11-23 13:31:16 +0000

    Implemented login and register component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,76 @@
-import FormLogin from "@/components/FormLogin";
+import { Button } from "@/components/ui/button";
+import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
-const page = () => {
+const Login = () => {
   return (
-    <div>
-      <FormLogin />
+    <div className=" text-basePrimary">
+      <CardHeader>
+        <CardTitle>Bem-vindo(a),</CardTitle>
+        <CardDescription className="text-baseDark">
+          Faça login para gerenciar sua conta e continuar conectado(a).
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" placeholder="Email" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Senha</Label>
+              <Input type="password" id="password" placeholder="Password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex flex-col w-full">
+        <Button type="submit" className="w-full">
+          Entrar
+        </Button>
+        <CardDescription>
+          Não tem uma conta?
+          <Link
+            href="/auth/register"
+            // onClick={onToggle}
+          >
+            <Button variant="link" className="w-min">
+              Cadastre-se
+            </Button>
+          </Link>
+        </CardDescription>
+        <div className="flex items-center space-x-2 w-full pb-2 md:pt-2">
+          <hr className="flex-grow border-1 border-my-primary" />
+          <span className="text-sm">OU</span>
+          <hr className="flex-grow border-1 border-my-primary" />
+        </div>
+        <Button
+          // onClick={() =>
+          //   signIn("google", {
+          //     callbackUrl: "http://localhost:3000/user/dashboard",
+          //   })
+          // }
+          variant="outline"
+          type="submit"
+          className="w-full"
+        >
+          <FcGoogle />
+          Entrar com Google
+        </Button>
+      </CardFooter>
     </div>
   );
 };
 
-export default page;
+export default Login;
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,81 @@
+import { Button } from "@/components/ui/button";
+import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
-const pageRegister = () => {
-  return <div>register</div>;
+const Register = () => {
+  return (
+    <div className="text-basePrimary">
+      <CardHeader>
+        <CardTitle>Criar Conta</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex gap-2">
+              <div className="flex flex-col space-y-1.5 w-full">
+                <Label htmlFor="name">Nome</Label>
+                <Input type="text" id="name" placeholder="Name" />
+              </div>
+              <div className="flex flex-col space-y-1.5 w-full">
+                <Label htmlFor="lastName">Sobrenome</Label>
+                <Input type="text" id="lastName" placeholder="Last Name" />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" placeholder="Email" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Senha</Label>
+              <Input type="password" id="password" placeholder="Password" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="confirmPassword">Confirmar Senha</Label>
+              <Input
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex flex-col w-full">
+        <Button type="submit" className="w-full">
+          Sign up
+        </Button>
+        <CardDescription className="">
+          Já tem uma conta?
+          <Link
+            href="/auth/login"
+            // onClick={onToggle}
+          >
+            <Button variant="link" className="w-min">
+              Cadastrar
+            </Button>
+          </Link>
+        </CardDescription>
+        <div className="flex items-center space-x-2 w-full pb-2 md:pt-2">
+          <hr className="flex-grow border-1 border-my-primary" />
+          <span className="text-sm">OU</span>
+          <hr className="flex-grow border-1 border-my-primary" />
+        </div>
+        <Button variant="outline" type="submit" className="w-full">
+          <FcGoogle /> Cadastrar com Google
+        </Button>
+      </CardFooter>
+    </div>
+  );
 };
 
-export default pageRegister;
+export default Register;
